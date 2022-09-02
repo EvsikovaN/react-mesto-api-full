62732e8e1e60ab41942e1cb2f7736e02ac51e798
EvsikovaN <evsikova32@gmail.com>
2022-09-02 16:13:38 +0300

    fix: urls

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -127,7 +127,6 @@ function App() {
     api
       .pushNewCard(data)
       .then((newCard) => {
-        // console.log(cards)
         setCards([newCard.card, ...cards]);
         closeAllPopups();
       })
